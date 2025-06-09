e101e65a79f01841be5c5dabdb025f568168b045
HudsonWayne <waynewaynebenhura@gmail.com>
2025-06-09 14:14:47 +0200

    working on operators

---
+++ demo/src/main/java/com/example/OperatorsDemo.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class OperatorsDemo {
+    public static void main(String[] args) {
+        int a = 30;
+        int b = 20;
+
+        System.out.println();
+    }
+}
