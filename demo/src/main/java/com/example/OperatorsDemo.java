6f1b2882a597338acc6466e8a4eee10c68d9a011
HudsonWayne <waynewaynebenhura@gmail.com>
2025-06-09 14:23:14 +0200

    working on operators

--- demo/src/main/java/com/example/OperatorsDemo.java
+++ demo/src/main/java/com/example/OperatorsDemo.java
@@ -9,5 +9,6 @@ public class OperatorsDemo {
         System.out.println("Subtraction: " + (a - b));
         System.out.println("Multiplication: " + (a * b));
         System.out.println("Division: " + (a / b));
+        System.out.println("Remainder: " + (a % b));
     }
 }
