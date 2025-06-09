b4612c375c32399429a86f3f176a30c334ceb5a8
HudsonWayne <waynewaynebenhura@gmail.com>
2025-06-09 14:06:18 +0200

    passing my variables

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -4,6 +4,11 @@ public class Main {
     public static void main(String[] args) {
         int age = 25;
         double height = 1.75;
+        String name = "John";
+        boolean isJave = true;
+
+
+
         System.out.println("Hello, Wayne! Welcome to Java!");
     }
 }
