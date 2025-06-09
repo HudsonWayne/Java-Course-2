cadfb9ce1a85b72af81518e0c097c58ab71f6471
HudsonWayne <waynewaynebenhura@gmail.com>
2025-06-09 14:05:26 +0200

    variables

---
+++ demo/src/main/java/com/example/Main.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int age = 25;
+        double height = 1.75;
+        System.out.println("Hello, Wayne! Welcome to Java!");
+    }
+}
