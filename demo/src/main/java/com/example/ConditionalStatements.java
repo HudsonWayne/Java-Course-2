05cb11d9a0b8b26e06125805953786b93c43aaf9
HudsonWayne <waynewaynebenhura@gmail.com>
2025-06-10 09:49:07 +0200

    initial commit

--- demo/src/main/java/com/example/ConditionalStatements.java
+++ demo/src/main/java/com/example/ConditionalStatements.java
@@ -15,7 +15,22 @@ public class ConditionalStatements {
         }
 
         int score = 75;
-        
+
+if (score >= 90) {
+    System.out.println("Grade: A");
+} else if (score >= 80) {
+    System.out.println("Grade: B");
+} else if (score >= 70) {
+    System.out.println("Grade: C");
+} else {
+    System.out.println("Grade: D");
+}
+
+
+      
+                
+            
+      
 
 
 
