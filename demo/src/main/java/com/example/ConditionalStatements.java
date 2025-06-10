3e54e9ef4647daf4352cc2fe0ab299bfcc507aa2
HudsonWayne <waynewaynebenhura@gmail.com>
2025-06-10 09:27:04 +0200

    conditional statements

---
+++ demo/src/main/java/com/example/ConditionalStatements.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class ConditionalStatements {
+    public static void main(String[] args) {
+        int age = 18;
+        if(age>=18){
+            System.out.println("You are an adult");
+        }
+    }
+}
